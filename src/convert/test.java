25ddbe5bc24b2cf5d288ca855d50e1c44bf722fc
miaopf <916969217@qq.com>
2018-04-10 15:39:46 +0800

    2

--- src/convert/test.java
+++ src/convert/test.java
@@ -6,7 +6,7 @@ public class test {
 	public static void main(String[] args){
 		ConvertUtil cu = new ConvertUtil();
 		try {
-			//1
+			//2
 			cu.Convert("doc", "F:/Cloudcc产品开发实现设计-天眼查-V1.0-20180131.doc", null, false);
 		} catch (Exception e) {
 			e.printStackTrace();
